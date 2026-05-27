64e33bc6103b6d82adbb8442e4639c6233b65e70
wall5009 <rolandjlindenfelser@gmail.com>
2026-05-26 20:49:02 -0700

    v1.1 again

--- common/src/main/java/com/rollylindenshnizzer/nexuscore/compat/CompatModule.java
+++ common/src/main/java/com/rollylindenshnizzer/nexuscore/compat/CompatModule.java
@@ -1,6 +1,5 @@
 package com.rollylindenshnizzer.nexuscore.compat;
 
-import com.rollylindenshnizzer.nexuscore.api.NexusDeprecated;
 import com.rollylindenshnizzer.nexuscore.core.NexusEnvironment;
 
 import java.util.Collection;
@@ -11,14 +10,12 @@ public interface CompatModule {
         return modId();
     }
 
-    @Deprecated(since = "1.1.0")
     String modId();
 
     default List<String> requiredMods() {
         return List.copyOf(dependencies());
     }
 
-    @NexusDeprecated(since = "1.1.0", replaceWith = "com.rollylindenshnizzer.nexuscore.compat.CompatModule.requiredMods")
     default Collection<String> dependencies() {
         return requiredMods();
     }
